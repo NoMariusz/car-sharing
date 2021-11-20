207ecc8953d375216cb212dbc5db0b08e23aed55
NoMariusz <mariusz2727@vp.pl>
2021-11-20 15:03:26 +0100

    Enabled to sort and to filter reservations

--- src/components/admin_panel/AdminReservationsBlock.svelte
+++ src/components/admin_panel/AdminReservationsBlock.svelte
@@ -16,6 +16,42 @@
     const updateReservation = (reservation) => {
         console.log(reservation);
     };
+
+    let sortBy = { col: "id", ascending: true };
+
+    const sortRes = (column) => {
+        if (sortBy.col == column) {
+            sortBy.ascending = !sortBy.ascending;
+        } else {
+            sortBy.col = column;
+            sortBy.ascending = true;
+        }
+
+        // Modifier to sorting function for ascending or descending
+        let sortModifier = sortBy.ascending ? 1 : -1;
+
+        let sort = (a, b) =>
+            a[column] < b[column]
+                ? -1 * sortModifier
+                : a[column] > b[column]
+                ? 1 * sortModifier
+                : 0;
+
+        reservations = reservations.sort(sort);
+    };
+
+    let filterObj = {};
+
+    $: filteredRes = reservations.filter((res) =>
+        Object.keys(filterObj).every(
+            (key) => res[key] == filterObj[key] || !filterObj[key]
+        )
+    );
+
+    $: getValuesForKey = (key) => [
+        "",
+        ...new Set(reservations.map((e) => e[key])),
+    ];
 </script>
 
 <div class="flex justify-center items-center">
@@ -31,6 +67,46 @@
                     Show, change reservation status, change reservation dates
                 </p>
             </div>
+
+            <div class="flex flex-wrap -m-2 lg:w-2/3 md:w-1/1 mx-auto">
+                <div
+                    class="flex justify-center items-center m-2 flex-wrap w-full"
+                >
+                    <p class="text-gray-900 title-font font-medium pb-1">
+                        Sort:
+                    </p>
+                    {#each ["id", "status_name", "car_id", "login", "start_date", "end_date", "return_date", "reputation"] as itemColumn}
+                        <button
+                            on:click={() => sortRes(itemColumn)}
+                            class="flex-1 p-1 m-1">{itemColumn}</button
+                        >
+                    {/each}
+                </div>
+                <div
+                    class="flex justify-center items-center m-2 flex-wrap w-full"
+                >
+                    <p class="text-gray-900 title-font font-medium pb-1">
+                        Filter:
+                    </p>
+                    {#each ["status_name", "login", "car_id", "cancelled"] as filterKey}
+                        <div class="flex flex-row m-3">
+                            <label for={filterKey}>
+                                {filterKey}
+                            </label>
+                            <select
+                                name="role"
+                                id="role"
+                                bind:value={filterObj[filterKey]}
+                            >
+                                {#each getValuesForKey(filterKey) as val}
+                                    <option value={val}>{val}</option>
+                                {/each}
+                            </select>
+                        </div>
+                    {/each}
+                </div>
+            </div>
+
             <div class="flex flex-wrap w-auto ">
                 <div class="w-auto overflow-auto mx-auto">
                     <table
@@ -54,7 +130,7 @@
                             </tr>
                         </thead>
                         <tbody>
-                            {#each reservations as res}
+                            {#each filteredRes as res}
                                 <tr
                                     class={`${
                                         res.cancelled == "1" ? "bg-red-50" : ""
